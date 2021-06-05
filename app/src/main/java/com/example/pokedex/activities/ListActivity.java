d2a41acbe7cac9e6997f3234b72ad7eea547b579
Felipe Sanchez <vegafelipe015@gmail.com>
2021-06-04 20:54:33 -0500

    Building adapter

--- app/src/main/java/com/example/pokedex/activities/ListActivity.java
+++ app/src/main/java/com/example/pokedex/activities/ListActivity.java
@@ -1,15 +1,19 @@
 package com.example.pokedex.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.pokedex.R;
+import com.example.pokedex.listLogic.PokemonAdapter;
 
 public class ListActivity extends AppCompatActivity {
 
     private RecyclerView pokemon_list;
+    private LinearLayoutManager layoutManager;
+    private PokemonAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,10 @@ public class ListActivity extends AppCompatActivity {
         pokemon_list =findViewById(R.id.pokemon_list);
         pokemon_list.setHasFixedSize(true);
 
+        layoutManager = new LinearLayoutManager(this);
+        pokemon_list.setLayoutManager(layoutManager);
+
+        adapter = new PokemonAdapter();
+        pokemon_list.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/pokedex/listLogic/PokemonAdapter.java
@@ -0,0 +1,11 @@
+package com.example.pokedex.listLogic;
+
+import com.example.pokedex.model.Pokemon;
+
+import java.util.ArrayList;
+
+public class PokemonAdapter {
+
+    private ArrayList<Pokemon> list;
+
+}
